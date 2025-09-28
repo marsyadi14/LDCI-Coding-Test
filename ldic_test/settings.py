5a3314cd1c63fe576f3f4ccfd34cd80bb6dc0b40
damiosdam <m.arsyad.ikbar.14@gmail.com>
2025-09-29 03:49:31 +0700

    Update

--- ldic_test/settings.py
+++ ldic_test/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,12 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-czhwsx&g4g!skll(npi0aau(w87y#44p273a6^mc^e_)_95a+5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    #"marsyadikbarlcdi.pythonanywhere.com"
+    "marsyadikbarlcdi.pythonanywhere.com"
     ]
-
 
 # Application definition
 
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,11 +5,12 @@ from .views import UserFormView, PostFormView, DashboardView, login_view, logout
 urlpatterns = [
     path("", DashboardView.as_view(), name="dashboard"),
     path("post/", RedirectView.as_view(url="/")),
-    path("post/<int:pk>/like/", toggle_like, name="post_like"),
     path("post/<int:pk>", DetailedPostView.as_view(), name="detailed_post"),
     path("login", login_view, name="login"),
     path("logout", logout_view, name="logout"),
     path("signup", UserFormView.as_view()),
+    
+    path("post/<int:pk>/like/", toggle_like, name="post_like"),
     
     path("api/posts", get_all_post_json, name="all_post"),
     path("api/posts/<int:post_id>", get_post_json, name="post_detailed")
